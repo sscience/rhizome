617ed22bb6962ce1535f9a7602c7f9acf3ac7025
John Dingee <sudoscience@yahoo.com>
2018-05-29 09:58:59 -0400

    adding full docker spec back in build

--- rhizome/settings.py
+++ rhizome/settings.py
@@ -24,7 +24,7 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = '/'
 
-SECRET_KEY = 'asfafasfasfasfasfsafafasfasfkaf' os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
