1457259c3aacf71bcf77533506a619c29c7501e4
github username <samson.yuwono@gmail.com>
2018-05-30 12:42:46 -0400

    want to see how server works in taskQueue

--- webapp/gulpfile.babel.js
+++ webapp/gulpfile.babel.js
@@ -1,32 +1,33 @@
-import gulp from 'gulp'
-import requireDir from 'require-dir'
-import gulpTaskConfig from './tasks/libs/gulp-task-config'
+import gulp from "gulp";
+import requireDir from "require-dir";
+import gulpTaskConfig from "./tasks/libs/gulp-task-config";
 
-gulpTaskConfig(gulp)
+gulpTaskConfig(gulp);
 
-requireDir('./tasks')
+requireDir("./tasks");
 
-gulp.config('base.root', '..')
-gulp.config('base.src', './src')
-gulp.config('base.dist', './public')
-gulp.config('templates', '../templates')
+gulp.config("base.root", "..");
+gulp.config("base.src", "./src");
+gulp.config("base.dist", "./public");
+gulp.config("templates", "../templates");
 
-gulp.config('tasks', requireDir('./tasks/config'))
+gulp.config("tasks", requireDir("./tasks/config"));
 
-gulp.config('tasks.build', {
+gulp.config("tasks.build", {
   taskQueue: [
     // 'clean', // Removed because i get this error: EBUSY: resource busy or locked, rmdir '/watcher-app/public/static'
-    'copy',
-    'sass',
-    'browserify',
-    'revCompile',
-    'revReplace'
+    "copy",
+    "sass",
+    "browserify",
+    "server",
+    "revCompile",
+    "revReplace"
   ]
-})
+});
 
-gulp.task('dev', () => {
-  gulp.config(gulp.DEV_MODE, true)
-  gulp.start(['build'])
-})
+gulp.task("dev", () => {
+  gulp.config(gulp.DEV_MODE, true);
+  gulp.start(["build"]);
+});
 
-gulp.task('default', ['build'])
+gulp.task("default", ["build"]);
