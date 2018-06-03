1b06f541aa4c9ef10eb37a0e1b8cb54322663852
John Dingee <sudoscience@yahoo.com>
2018-06-03 13:40:41 -0400

    rm cache control header, add chart create html

--- webapp/src/actions/RootActions.js
+++ webapp/src/actions/RootActions.js
@@ -7,6 +7,8 @@ const RootActions = Reflux.createActions({
 
 // API CALLS
 // ---------------------------------------------------------------------------
-RootActions.fetchAllMeta.listenAndPromise(() => api.get_all_meta(null, null, {'cache-control': 'no-cache'}))
+// RootActions.fetchAllMeta.listenAndPromise(() => api.get_all_meta(null, null, {'cache-control': 'no-cache'}))
+RootActions.fetchAllMeta.listenAndPromise(() => api.get_all_meta(null, null}))
+
 
 export default RootActions
